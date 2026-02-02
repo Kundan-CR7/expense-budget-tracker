4a690bfd536b8cd7e671e3af4b58e37ad2a76b51
Kundan-CR7 <Kundangupta7334@gmail.com>
2026-02-02 23:34:42 +0530

    add /api routes

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import { ExpenseRoutes } from "./routes/expense.routes";
 
 const app = express();
-
 app.use(express.json());
+
+const expenseRoutes = new ExpenseRoutes();
+app.use("/api", expenseRoutes.router);
 
 app.get("/", (_, res) => {
   res.send("Expense Tracker API is running 🚀");
