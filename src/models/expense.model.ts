a57e9899ea5f6bc79d0eed6405908fbea151d790
Kundan-CR7 <Kundangupta7334@gmail.com>
2026-02-02 23:50:33 +0530

    add filters in findAll function

--- src/models/expense.model.ts
+++ src/models/expense.model.ts
@@ -7,10 +7,47 @@ export class ExpenseModel {
         this.expenses.push(expense);
         return expense;
     }
+    findAll(filters: any) {
+        let results = [...this.expenses];
 
-    findAll(): Expense[] {
-        return this.expenses;
+        if (filters.search) {
+            results = results.filter(exp =>
+            exp.title.toLowerCase().includes(filters.search.toLowerCase())
+            );
+        }
+
+        if (filters.category) {
+            results = results.filter(exp => exp.category === filters.category);
+        }
+
+        if (filters.minAmount) {
+            results = results.filter(exp => exp.amount >= Number(filters.minAmount));
+        }
+
+        if (filters.maxAmount) {
+            results = results.filter(exp => exp.amount <= Number(filters.maxAmount));
+        }
+
+        if (filters.sortBy) {
+            results.sort((a, b) => {
+            const order = filters.order === "desc" ? -1 : 1;
+            return a[filters.sortBy] > b[filters.sortBy] ? order : -order;
+            });
+        }
+
+        const page = Number(filters.page) || 1;
+        const limit = Number(filters.limit) || 10;
+        const start = (page - 1) * limit;
+        const end = start + limit;
+
+        return {
+            total: results.length,
+            page,
+            limit,
+            data: results.slice(start, end)
+        };
     }
+
 
     findById(id: string): Expense | undefined {
         return this.expenses.find(exp => exp.id === id);
